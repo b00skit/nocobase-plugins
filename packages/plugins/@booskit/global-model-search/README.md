096e6ccf3cfb2a40c69fa1a8746eb9c0439427f9
Biscuit <gtaw@booskit.dev>
2026-03-11 14:53:49 +0100

    not ready

--- packages/plugins/@booskit/global-model-search/README.md
+++
@@ -1,1 +0,0 @@
-# @booskit/global-model-search
--- packages/plugins/@booskit/global-model-search/dist/client/index.d.ts
+++
@@ -1,7 +0,0 @@
-import { Plugin } from '@nocobase/client';
-export declare class GlobalModelSearchClient extends Plugin {
-    afterAdd(): Promise<void>;
-    beforeLoad(): Promise<void>;
-    load(): Promise<void>;
-}
-export default GlobalModelSearchClient;
